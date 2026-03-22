6f2bd5d060f4b51ca591aaaf25b139be5e5ca98f
diszexuf <diszexuf@yandex.ru>
2026-03-22 15:25:36 +0500

    feat: add message.xsd and generating script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+val xmlbeansConfig by configurations.creating
+
 plugins {
     id("java")
 }
@@ -10,6 +12,10 @@ repositories {
 }
 
 dependencies {
+    xmlbeansConfig("org.apache.xmlbeans:xmlbeans:5.3.0")
+    implementation(files("lib/schema.jar"))
+    implementation("org.xerial:sqlite-jdbc:3.51.3.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -17,4 +23,25 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register<JavaExec>("runScomp") {
+    group = "build"
+    description = "Генерация из message.xsd"
+
+    classpath = xmlbeansConfig
+    mainClass.set("org.apache.xmlbeans.impl.tool.SchemaCompiler")
+
+    inputs.file(file("schema/message.xsd"))
+    outputs.file(file("lib/schema.jar"))
+
+    args(
+        "-out", file("lib/schema.jar").absolutePath,
+        "-d", file("${layout.buildDirectory.get()}/generated/classes").absolutePath,
+        file("schema/message.xsd").absolutePath
+    )
+}
+
+tasks.named("compileJava") {
+    dependsOn("runScomp")
 }
